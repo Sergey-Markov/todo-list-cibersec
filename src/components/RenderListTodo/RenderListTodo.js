911974627457f3f590d42b6a0efb2127d4105fe2
Sergey <1nativefl@gmail.com>
2022-04-07 13:52:55 +0300

    add component TodoItem

--- src/components/RenderListTodo/RenderListTodo.js
+++ src/components/RenderListTodo/RenderListTodo.js
@@ -1,7 +1,4 @@
-import { useState, useCallback } from "react";
-import { Button } from "react-bootstrap";
-import { RiDeleteBin5Line, RiChatQuoteLine } from "react-icons/ri";
-import { MdOutlineDoneAll } from "react-icons/md";
+import TodoItem from "../TodoItem/TodoItem";
 
 import s from "./RenderListTodo.module.css";
 
@@ -13,50 +10,18 @@ const RenderListTodo = ({
   handleClickCompleteBtn,
   handleChangeTextTodoBtn,
 }) => {
-  const activeStyleClassTodo = s.list_item_text;
-  const completedStyleClassTodo = s.list_item_text_completed;
-
   return (
     <ol className={s.field_list}>
       {todos.map((todo) => {
         return (
-          <li key={todo.id} className={s.list_item_field}>
-            <p
-              className={
-                !todo.completed ? activeStyleClassTodo : completedStyleClassTodo
-              }
-            >
-              {todo.title}
-            </p>
-            <div>
-              <Button
-                type="button"
-                className={s.ctrlBtn}
-                variant="primary"
-                onClick={() => {
-                  handleClickDeleteBtn(todo.id);
-                }}
-              >
-                <RiDeleteBin5Line />
-              </Button>
-              <Button
-                type="button"
-                className={s.ctrlBtn}
-                variant="primary"
-                onClick={() => handleChangeTextTodoBtn(todo)}
-              >
-                <RiChatQuoteLine />
-              </Button>
-              <Button
-                type="button"
-                className={s.ctrlBtn}
-                variant="primary"
-                onClick={() => handleClickCompleteBtn(todo.id)}
-              >
-                <MdOutlineDoneAll />
-              </Button>
-            </div>
-          </li>
+          <TodoItem
+            todo={todo}
+            handleClickDeleteBtn={handleClickDeleteBtn}
+            handleClickCompleteBtn={handleClickCompleteBtn}
+            handleChangeTextTodoBtn={handleChangeTextTodoBtn}
+            toggleModal={toggleModal}
+            setSomeTodo={setSomeTodo}
+          />
         );
       })}
     </ol>
